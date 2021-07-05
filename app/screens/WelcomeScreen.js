b3f34eb1732bcd07bcb618a27b224c7474a61ddc
Alexander Schulte <alex11.schulte@googlemail.com>
2021-07-05 13:47:18 +0200

    Test an Icon and create AppText.js

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,6 +1,9 @@
 import React from "react";
 import colors from "../config/colors";
 import { ImageBackground, StyleSheet, View, Image } from "react-native";
+import AppText from "../components/AppText";
+import { Ionicons } from "@expo/vector-icons";
+import { white } from "color-name";
 
 function WelcomeScreen(props) {
   return (
@@ -9,6 +12,10 @@ function WelcomeScreen(props) {
       source={require("../assets/Background.jpg")}
     >
       {/* <Image source={require("../assets/icon.png")} /> */}
+
+      <AppText>I love my Court!</AppText>
+      <Ionicons name="basketball-outline" size={20} />
+
       <View style={styles.loginButton}></View>
       <View style={styles.registerButton}></View>
     </ImageBackground>
@@ -19,6 +26,9 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
     justifyContent: "flex-end",
+  },
+  text: {
+    color: "#fff",
   },
   loginButton: {
     width: "100%",
