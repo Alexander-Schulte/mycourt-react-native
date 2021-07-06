5a59c13ef5de12d29f05d9b987561f15853ccc68
Alexander Schulte <alex11.schulte@googlemail.com>
2021-07-06 19:57:51 +0200

    Implement Text, Buttons, Styling to WelcomeScreen.js

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,23 +1,26 @@
 import React from "react";
 import colors from "../config/colors";
-import { ImageBackground, StyleSheet, View, Image } from "react-native";
+import { ImageBackground, StyleSheet, View, Text, Image } from "react-native";
 import AppText from "../components/AppText";
 import { Ionicons } from "@expo/vector-icons";
-import { white } from "color-name";
+import AppButton from "../components/AppButton";
 
 function WelcomeScreen(props) {
   return (
     <ImageBackground
+      blurRadius={4}
       style={styles.background}
       source={require("../assets/Background.jpg")}
     >
       {/* <Image source={require("../assets/icon.png")} /> */}
-
-      <AppText>I love my Court!</AppText>
-      <Ionicons name="basketball-outline" size={20} />
-
-      <View style={styles.loginButton}></View>
-      <View style={styles.registerButton}></View>
+      <View style={styles.logoContainer}>
+        <Ionicons style={styles.logo} name="basketball-outline" size={60} />
+        <Text style={styles.tagline}>Find your next court</Text>
+      </View>
+      <View style={styles.buttonsContainer}>
+        <AppButton title="Login" onPress={() => console.log("Tapped")} />
+        <AppButton title="Register" color="seccondary" />
+      </View>
     </ImageBackground>
   );
 }
@@ -26,19 +29,26 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
     justifyContent: "flex-end",
+    alignItems: "center",
   },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+  tagline: {
+    fontSize: 25,
+    fontWeight: "600",
+    paddingVertical: 15,
+  },
+  buttonsContainer: {
+    padding: 20,
+    marginBottom: 10,
+    width: "100%",
+  },
+
   text: {
     color: "#fff",
-  },
-  loginButton: {
-    width: "100%",
-    height: 70,
-    backgroundColor: colors.primary,
-  },
-  registerButton: {
-    width: "100%",
-    height: 70,
-    backgroundColor: colors.seccondary,
   },
 });
 
