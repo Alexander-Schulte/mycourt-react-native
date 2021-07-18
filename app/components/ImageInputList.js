f8ec029c937ea5d83f6fe5cbfd1fb85a8d1f9427
Alexander Schulte <alex11.schulte@googlemail.com>
2021-07-18 14:31:17 +0200

    Create FormImagePicker.js and implement it to my Form

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,16 +1,29 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useRef } from "react";
+import { View, StyleSheet, ScrollView } from "react-native";
 import ImageInput from "./ImageInput";
 
 function ImageInputList({ imageUris = [], onRemoveImage, onAddImage }) {
+  const scrollView = useRef();
+
   return (
-    <View style={styles.container}>
-      {imageUris.map((uri) => (
-        <View key={uri} style={styles.image}>
-          <ImageInput imageUri={uri} onChangeImage={() => onRemoveImage(uri)} />
+    <View>
+      <ScrollView
+        ref={scrollView}
+        horizontal
+        onContentSizeChange={() => scrollView.current.scrollToEnd()}
+      >
+        <View style={styles.container}>
+          {imageUris.map((uri) => (
+            <View key={uri} style={styles.image}>
+              <ImageInput
+                imageUri={uri}
+                onChangeImage={() => onRemoveImage(uri)}
+              />
+            </View>
+          ))}
+          <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
         </View>
-      ))}
-      <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
+      </ScrollView>
     </View>
   );
 }
@@ -19,7 +32,9 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
   },
-  image: { marginRight: 10 },
+  image: {
+    marginRight: 10,
+  },
 });
 
 export default ImageInputList;
