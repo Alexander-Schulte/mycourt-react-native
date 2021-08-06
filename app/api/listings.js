a05e3e09e4d06249e4e9d6b8193efbfca47fc3eb
Alexander Schulte <alex11.schulte@googlemail.com>
2021-08-06 12:03:33 +0200

    Still server Problems

--- app/api/listings.js
+++ app/api/listings.js
@@ -1,45 +1,12 @@
-// import apiClient from "./client";
-
-// const endpoint = "/listings";
-
-// const getListings = () => apiClient.get(endpoint);
-
-// export const addListing = (listing) => {
-//   const data = new FormData();
-//   data.append("address", listing.address);
-//   data.append("price", listing.price);
-//   data.append("categoryId", listing.category.value);
-//   data.append("description", listing.description);
-
-//   listing.images.forEach((image, index) =>
-//     data.append("images", {
-//       name: "image" + index,
-//       type: "image/jpeg",
-//       uri: image,
-//     })
-//   );
-//   if (listing.location)
-//     data.append("location", JSON.stringify(listing.location));
-
-//   console.log(data);
-
-//   return apiClient.post(endpoint, data);
-// };
-
-// export default {
-//   addListing,
-//   getListings,
-// };
-
-import client from "./client";
+import apiClient from "./client";
 
 const endpoint = "/listings";
 
-const getListings = () => client.get(endpoint);
+const getListings = () => apiClient.get(endpoint);
 
-export const addListing = (listing, onUploadProgress) => {
+export const addListing = (listing) => {
   const data = new FormData();
-  data.append("title", listing.title);
+  data.append("address", listing.address);
   data.append("price", listing.price);
   data.append("categoryId", listing.category.value);
   data.append("description", listing.description);
@@ -51,14 +18,12 @@ export const addListing = (listing, onUploadProgress) => {
       uri: image,
     })
   );
-
   if (listing.location)
     data.append("location", JSON.stringify(listing.location));
 
-  return client.post(endpoint, data, {
-    onUploadProgress: (progress) =>
-      onUploadProgress(progress.loaded / progress.total),
-  });
+  console.log(data);
+
+  return apiClient.post(endpoint, data);
 };
 
 export default {
