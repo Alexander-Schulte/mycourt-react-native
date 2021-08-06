c652846e4311f7994b8b5dde3a5ca29029660c5c
Alexander Schulte <alex11.schulte@googlemail.com>
2021-08-06 12:01:33 +0200

    Server problems

--- app/api/listings.js
+++ app/api/listings.js
@@ -1,12 +1,45 @@
-import apiClient from "./client";
+// import apiClient from "./client";
+
+// const endpoint = "/listings";
+
+// const getListings = () => apiClient.get(endpoint);
+
+// export const addListing = (listing) => {
+//   const data = new FormData();
+//   data.append("address", listing.address);
+//   data.append("price", listing.price);
+//   data.append("categoryId", listing.category.value);
+//   data.append("description", listing.description);
+
+//   listing.images.forEach((image, index) =>
+//     data.append("images", {
+//       name: "image" + index,
+//       type: "image/jpeg",
+//       uri: image,
+//     })
+//   );
+//   if (listing.location)
+//     data.append("location", JSON.stringify(listing.location));
+
+//   console.log(data);
+
+//   return apiClient.post(endpoint, data);
+// };
+
+// export default {
+//   addListing,
+//   getListings,
+// };
+
+import client from "./client";
 
 const endpoint = "/listings";
 
-const getListings = () => apiClient.get(endpoint);
+const getListings = () => client.get(endpoint);
 
-export const addListing = (listing) => {
+export const addListing = (listing, onUploadProgress) => {
   const data = new FormData();
-  data.append("address", listing.address);
+  data.append("title", listing.title);
   data.append("price", listing.price);
   data.append("categoryId", listing.category.value);
   data.append("description", listing.description);
@@ -18,12 +51,14 @@ export const addListing = (listing) => {
       uri: image,
     })
   );
+
   if (listing.location)
     data.append("location", JSON.stringify(listing.location));
 
-  console.log(data);
-
-  return apiClient.post(endpoint, data);
+  return client.post(endpoint, data, {
+    onUploadProgress: (progress) =>
+      onUploadProgress(progress.loaded / progress.total),
+  });
 };
 
 export default {
--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,3 +1,123 @@
+// import React from "react";
+// import { StyleSheet } from "react-native";
+
+// import * as Yup from "yup";
+
+// import {
+//   AppForm,
+//   AppFormField,
+//   AppFormPicker,
+//   SubmitButton,
+// } from "../components/forms";
+// import colors from "../config/colors";
+// import CategoryPickerItem from "../components/CategoryPickerItem";
+// import Screen from "../components/Screen";
+// import FormImagePicker from "../components/forms/FormImagePicker";
+// import listingsApi from "../api/listings";
+// import useLocation from "../hooks/useLocation";
+
+// const validationSchema = Yup.object().shape({
+//   address: Yup.string().required().min(1).label("Address"),
+//   price: Yup.number().required().min(1).max(10000).label("Price"),
+//   category: Yup.object().required().nullable().label("Category"),
+//   description: Yup.string().label("Description"),
+//   images: Yup.array(),
+// });
+
+// const categories = [
+//   {
+//     label: "Basketball",
+//     value: 1,
+//     backgroundColor: "orange",
+//     icon: "basketball",
+//   },
+//   {
+//     label: "Volleyball",
+//     value: 2,
+//     backgroundColor: colors.secondary,
+//     icon: "volleyball",
+//   },
+//   { label: "Tennis", value: 3, backgroundColor: colors.medium, icon: "tennis" },
+//   {
+//     label: "Football",
+//     value: 4,
+//     backgroundColor: "limegreen",
+//     icon: "football",
+//   },
+//   {
+//     label: "Badminton",
+//     value: 5,
+//     backgroundColor: "green",
+//     icon: "badminton",
+//   },
+//   {
+//     label: "Soccer",
+//     value: 6,
+//     backgroundColor: "tomato",
+//     icon: "soccer",
+//   },
+// ];
+
+// function ListingEditScreen() {
+//   // const location = useLocation();
+
+//   const handleSubmit = async (listing) => {
+//     const result = await listingsApi.addListing(listing);
+//     if (!result.ok)
+//       return console.log(result.problem), alert("Could not save the listing");
+//     alert("Success");
+//   };
+
+//   return (
+//     <Screen style={styles.container}>
+//       <AppForm
+//         initialValues={{
+//           address: "",
+//           price: "",
+//           description: "",
+//           category: null,
+//           images: [],
+//         }}
+//         onSubmit={handleSubmit}
+//         validationSchema={validationSchema}
+//       >
+//         <FormImagePicker name="images" />
+//         <AppFormField maxLength={100} name="address" placeholder="Address" />
+//         <AppFormField
+//           keyboardType="numeric"
+//           maxLength={8}
+//           name="price"
+//           width={120}
+//           placeholder="Price"
+//         />
+//         <AppFormPicker
+//           items={categories}
+//           numberOfColumns={3}
+//           PickerItemComponent={CategoryPickerItem}
+//           name="category"
+//           placeholder="Category"
+//           width="50%"
+//         />
+//         <AppFormField
+//           maxLength={255}
+//           multiline
+//           name="description"
+//           numberOfLines={3}
+//           placeholder="Description"
+//         />
+//         <SubmitButton title="Post" />
+//       </AppForm>
+//     </Screen>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     padding: 10,
+//   },
+// });
+// export default ListingEditScreen;
+
 import React from "react";
 import { StyleSheet } from "react-native";
 
@@ -17,44 +137,67 @@ import listingsApi from "../api/listings";
 import useLocation from "../hooks/useLocation";
 
 const validationSchema = Yup.object().shape({
-  address: Yup.string().required().min(1).label("Address"),
+  title: Yup.string().required().min(1).label("Title"),
   price: Yup.number().required().min(1).max(10000).label("Price"),
-  category: Yup.object().required().nullable().label("Category"),
   description: Yup.string().label("Description"),
-  images: Yup.array(),
+  category: Yup.object().required().nullable().label("Category"),
+  images: Yup.array().min(1, "Please select at least one image."),
 });
 
 const categories = [
   {
-    label: "Basketball",
+    backgroundColor: "#fc5c65",
+    icon: "floor-lamp",
+    label: "Furniture",
     value: 1,
-    backgroundColor: "orange",
-    icon: "basketball",
   },
   {
-    label: "Volleyball",
+    backgroundColor: "#fd9644",
+    icon: "car",
+    label: "Cars",
     value: 2,
-    backgroundColor: colors.secondary,
-    icon: "volleyball",
   },
-  { label: "Tennis", value: 3, backgroundColor: colors.medium, icon: "tennis" },
   {
-    label: "Football",
+    backgroundColor: "#fed330",
+    icon: "camera",
+    label: "Cameras",
+    value: 3,
+  },
+  {
+    backgroundColor: "#26de81",
+    icon: "cards",
+    label: "Games",
     value: 4,
-    backgroundColor: "limegreen",
-    icon: "football",
   },
   {
-    label: "Badminton",
+    backgroundColor: "#2bcbba",
+    icon: "shoe-heel",
+    label: "Clothing",
     value: 5,
-    backgroundColor: "green",
-    icon: "badminton",
   },
   {
-    label: "Soccer",
+    backgroundColor: "#45aaf2",
+    icon: "basketball",
+    label: "Sports",
     value: 6,
-    backgroundColor: "tomato",
-    icon: "soccer",
+  },
+  {
+    backgroundColor: "#4b7bec",
+    icon: "headphones",
+    label: "Movies & Music",
+    value: 7,
+  },
+  {
+    backgroundColor: "#a55eea",
+    icon: "book-open-variant",
+    label: "Books",
+    value: 8,
+  },
+  {
+    backgroundColor: "#778ca3",
+    icon: "application",
+    label: "Other",
+    value: 9,
   },
 ];
 
@@ -72,7 +215,7 @@ function ListingEditScreen() {
     <Screen style={styles.container}>
       <AppForm
         initialValues={{
-          address: "",
+          title: "",
           price: "",
           description: "",
           category: null,
@@ -82,19 +225,19 @@ function ListingEditScreen() {
         validationSchema={validationSchema}
       >
         <FormImagePicker name="images" />
-        <AppFormField maxLength={100} name="address" placeholder="Address" />
+        <AppFormField maxLength={255} name="title" placeholder="Title" />
         <AppFormField
           keyboardType="numeric"
           maxLength={8}
           name="price"
-          width={120}
           placeholder="Price"
+          width={120}
         />
         <AppFormPicker
           items={categories}
+          name="category"
           numberOfColumns={3}
           PickerItemComponent={CategoryPickerItem}
-          name="category"
           placeholder="Category"
           width="50%"
         />
